cc01d47514578c8a293bb24e36d275ea4c677c8d
TheDrianN <edelgadovidarte@gmail.com>
2024-05-30 21:55:11 -0500

    TheDrianN V2

--- src/components/TablaJurado.tsx
+++ src/components/TablaJurado.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import DataTable from './DataTable';
+import DataTable, { TableColumn } from 'react-data-table-component';
 import axios from 'axios';
 
-interface Jurado {
+// Define un tipo genérico para tus datos
+type DataItem = {
   jurado_id: number;
   nombre_completo: string;
   email: string;
@@ -10,10 +11,10 @@ interface Jurado {
   semestre: string;
   dedicacion: string;
   hora_asesoria_semanal: number;
-}
+};
 
 const TablaJurado: React.FC = () => {
-  const [data, setData] = useState<Jurado[]>([]);
+  const [data, setData] = useState<DataItem[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -22,9 +23,7 @@ const TablaJurado: React.FC = () => {
         const response = await axios.get('http://127.0.0.1:5000/jurados/obtener-jurado-semestre');
         const jsonData = JSON.parse(response.data);
         const data = jsonData.data;
-        console.log(data)
-        setData(data); // Asigna los datos de la respuesta al estado 'data'
-        
+        setData(data);
         setLoading(false); // Cambia el estado de carga a falso
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -34,39 +33,37 @@ const TablaJurado: React.FC = () => {
     fetchData(); // Llama a la función fetchData al montar el componente
   }, []);
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'ID',
-        accessor: 'jurado_id',
-      },
-      {
-        Header: 'Nombre',
-        accessor: 'nombre_completo',
-      },
-      {
-        Header: 'Email',
-        accessor: 'email',
-      },
-      {
-        Header: 'Teléfono',
-        accessor: 'telefono',
-      },
-      {
-        Header: 'Semestre',
-        accessor: 'semestre',
-      },
-      {
-        Header: 'Dedicación',
-        accessor: 'dedicacion',
-      },
-      {
-        Header: 'Hora de Asesoría Semanal',
-        accessor: 'hora_asesoria_semanal',
-      },
-    ],
-    []
-  );
+  const columns: TableColumn<DataItem>[] = [
+    {
+      name: 'ID',
+      selector: (row: DataItem) => row.jurado_id,
+      sortable: true
+    },
+    {
+      name: 'Nombre',
+      selector: (row: DataItem) => row.nombre_completo,
+    },
+    {
+      name: 'Email',
+      selector: (row: DataItem) => row.email,
+    },
+    {
+      name: 'Teléfono',
+      selector: (row: DataItem) => row.telefono,
+    },
+    {
+      name: 'Semestre',
+      selector: (row: DataItem) => row.semestre,
+    },
+    {
+      name: 'Dedicación',
+      selector: (row: DataItem) => row.dedicacion,
+    },
+    {
+      name: 'Hora de Asesoría Semanal',
+      selector: (row: DataItem) => row.hora_asesoria_semanal,
+    },
+  ];
 
   return (
     <div className='mt-5 flex flex-col w-full max-w-lg mx-auto bg-white rounded-xl shadow-md overflow-hidden p-5'>
@@ -74,7 +71,7 @@ const TablaJurado: React.FC = () => {
       {loading ? (
         <p>Cargando...</p>
       ) : (
-        <DataTable columns={columns} data={data} />
+        <DataTable className='text-color-black' columns={columns} data={data} />
       )}
     </div>
   );
