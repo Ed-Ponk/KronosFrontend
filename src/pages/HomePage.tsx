69e33e5ba351588c806619233cf73ef1e1cb5210
TheDrianN <edelgadovidarte@gmail.com>
2024-06-05 00:04:05 -0500

    Home para Whatsapp

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
+import FormHomeWSP from "../components/Home/FormHomeWSP";
 
 
 
 const HomePage = ()=>{
-  return(
-    <div>
-
+  return (
+    <div className='pt-5 w-full  overflow-auto h-full md:w-auto bg-gray-100 dark:bg-slate-900 '>
+      <FormHomeWSP />
     </div>
-  )
+  );
+
 };
 
 
